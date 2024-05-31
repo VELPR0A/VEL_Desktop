234d33bc8eddfd308885fca417d2370928db2a55
Diogo Antonny <Diogojp202@gmail.com>
2024-05-31 14:47:21 -0300

    Css desenvolvido

--- ambiente/src/components/TelaPagamento/TelaPagamento.jsx
+++ ambiente/src/components/TelaPagamento/TelaPagamento.jsx
@@ -1,6 +1,7 @@
 import "../../assets/styles/TelaPagamento.css";
 import { FaCreditCard, FaPaypal, FaPix } from "react-icons/fa6";
 import { FaCheckCircle, FaCheck } from "react-icons/fa";
+import { CiDiscount1 } from "react-icons/ci";
 
 export default function TelaPagamento(){
     const CartaoPagamento = ({icone, titulo, texto}) => {
@@ -22,20 +23,40 @@ export default function TelaPagamento(){
                 <article>
                     <form action="#" method="POST"> {/*Colocar url do Backend*/}
                         <h2>Complete seu cadastro na <span className="laranja">VEL</span></h2>
-                        <label htmlFor="IRazaoSocial">Razão Social</label>
-                        <input type="text" name="RazaoSocial" id="IRazaoSocial" />
-                    
-                        <label htmlFor="ICNPJ">CNPJ</label>
-                        <input type="number" name="CNPJ" id="ICNPJ" />
-                        <br />
-                        <label htmlFor="IEmail">Email</label>
-                        <input type="email" name="Email" id="IEmail" />
-                        <label htmlFor="ITelefone">Telefone</label>
-                        <input type="tel" name="Telefone" id="ITelefone" />
-                        <br />
-                        <label htmlFor="ICupom">Possui algum cupom ?</label>
-                        <input type="text" name="Cupom" id="ICupom" />
-                        <input type="button" value="REMOVER" />
+                        <div>
+                            <div>
+                                <div>
+                                    <label htmlFor="IRazaoSocial">Razão Social</label>
+                                    <input type="text" name="RazaoSocial" id="IRazaoSocial" />
+                                </div>
+                                <div>
+                                    <label htmlFor="ICNPJ">CNPJ</label>
+                                    <input type="number" name="CNPJ" id="ICNPJ" />
+                                </div>
+                            </div>
+                            
+                            <div>
+                                <div>
+                                    <label htmlFor="IEmail">Email</label>
+                                    <input type="email" name="Email" id="IEmail" />
+                                </div>
+                                <div>
+                                    <label htmlFor="ITelefone">Telefone</label>
+                                    <input type="tel" name="Telefone" id="ITelefone" placeholder="(00) 0 0000-0000" />
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label htmlFor="ICupom">
+                                        <CiDiscount1 />
+                                        Possui algum cupom ?</label>
+                                    <div>
+                                        <input type="text" name="Cupom" id="ICupom" placeholder="porTodaVel60" />
+                                        <input type="button" value="REMOVER" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                     <section>
                         <h2>Como será o pagamento ?</h2>
@@ -44,26 +65,32 @@ export default function TelaPagamento(){
                         <CartaoPagamento icone={<FaPix />} titulo="Pix" texto="à vista" />
                         <CartaoPagamento icone={<FaCreditCard />} titulo="Cartão de Débito" texto="à vista" />
                     </section>
+                    <footer>
+                        <hr />
+                        <p>
+                            <FaCheckCircle /> Nós da Virtual Easy Log estamos comprometidos em oferecer uma ótima experiência. Por isso oferecemos uma garantia de 7 dias ou seu dinheiro de volta.
+                        </p>
+                    </footer>
                 </article>
                 <aside>
                     <h2>Resumo</h2>
                     <p><span>Subtotal</span> <span>R$ 0/mês</span></p>
                     <p><span>TOTAL</span> <span><strong>R$ 0</strong> <br /> em 12x de R$ 0</span></p>
                     <div>
-                        <h4>TRANSFORME</h4>
-                        <ul>
-                            <li><FaCheck /> Acesso a toda nossa plataforma</li>
-                            <li><FaCheck /> Cadastro ilimitado de entregadores</li>
-                        </ul>
+                        <div>
+                            <h4>TRANSFORME</h4>
+                            <ul>
+                                <li><FaCheck /> Acesso a toda nossa plataforma</li>
+                                <li><FaCheck /> Cadastro ilimitado de entregadores</li>
+                            </ul>
+                        </div>
                         <button>CONFIRMAR PAGAMENTO</button>
-                        <p>Ao preencher o formulário você permite que a Virtual Easy Log salve suas informações.</p>
+                        <p>
+                            Ao preencher o formulário você permite que a Virtual Easy Log salve suas informações.
+                        </p>
                     </div>
                 </aside>
             </main>
-            <footer>
-                <hr />
-                <p><FaCheckCircle /> Nós da Virtual Easy Log estamos comprometidos em oferecer uma ótima experiência. Por isso oferecemos uma garantia de 7 dias ou seu dinheiro de volta.</p>
-            </footer>
         </>
     );
 } 
