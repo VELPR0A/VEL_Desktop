ab6643725cba424d6d9c4fcac260cc53294fa053
Diogo Antonny <Diogojp202@gmail.com>
2024-06-05 21:41:18 -0300

    Arquivo de teste criado

--- ambiente/src/App.jsx
+++ ambiente/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <>
-    <h1>Tela inicial</h1>
+      <h1>Tela inicial</h1>
+
       <a href="../pages/paginaPagamento.html">Tela De pagamento aqui</a>
+      <br />
+      <a href="../pages/paginaTeste.html">Tela teste aqui!</a>
     </>
   )
 }
---
+++ ambiente/src/Paginas/PaginaTeste/app.jsx
@@ -0,0 +1,11 @@
+import HelloWorld from "../../components/PaginaTeste/HelloWorld.jsx"
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App;
